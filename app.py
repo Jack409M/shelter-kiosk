abcfd786510430268bef2edb94cb7c0746767730
Jack409M <admin@dwcenter.org>
2026-03-01 12:59:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta, timezone
 from functools import wraps
 from typing import Any, Optional
 
-from flask import Flask, g, redirect, render_template, request, session, url_for, flash
+from flask import Flask, g, redirect, render_template, request, session, url_for, flash, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from zoneinfo import ZoneInfo
 
@@ -2280,6 +2280,7 @@ if __name__ == "__main__":
     with app.app_context():
         init_db()
     app.run(host="127.0.0.1", port=5000)
+
 
 
 
