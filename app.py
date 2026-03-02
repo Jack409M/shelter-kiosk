0734be0041daf2276557b122ddce2d6bc2d23fed
Jack409M <admin@dwcenter.org>
2026-03-02 10:47:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1490,7 +1490,6 @@ def staff_leave_approve(req_id: int):
 @require_login
 @require_shelter
 @require_transfer
-@require_transfer
 def staff_leave_deny(req_id: int):
     shelter = session["shelter"]
     staff_id = session["staff_user_id"]
@@ -2613,6 +2612,7 @@ if __name__ == "__main__":
     with app.app_context():
         init_db()
     app.run(host="127.0.0.1", port=5000)
+
 
 
 
