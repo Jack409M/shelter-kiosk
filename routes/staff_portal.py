9f4b02ddccd88c42a9fba2f41cce5ad1b320cace
Jack409M <admin@dwcenter.org>
2026-03-06 19:30:17 -0600

    Update staff_portal.py

--- routes/staff_portal.py
+++ routes/staff_portal.py
@@ -1,3 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 staff_portal = Blueprint("staff_portal", __name__)
+
+
+@staff_portal.route("/_staff_test/attendance")
+def staff_attendance_test():
+    return "staff attendance blueprint working"
