e88d18ebc41103233d82b60fc82af74db11bcc97
Jack409M <admin@dwcenter.org>
2026-03-06 20:01:20 -0600

    Update auth.py

--- core/auth.py
+++ core/auth.py
@@ -5,7 +5,7 @@ from flask import session, redirect, url_for
 def require_login(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        if "staff_id" not in session:
+        if "staff_user_id" not in session:
             return redirect(url_for("staff_login"))
         return f(*args, **kwargs)
 
