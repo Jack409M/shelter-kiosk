f0be7025731d5560ecdf9e5a15541c8b243d5a5f
Jack409M <admin@dwcenter.org>
2026-03-06 20:09:07 -0600

    Update auth.py

--- core/auth.py
+++ core/auth.py
@@ -10,3 +10,10 @@ def require_login(f):
         return f(*args, **kwargs)
 
     return wrapper
+
+def require_shelter(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        if "shelter" not in session:
+            return redirect(url_for("staff_select_shelter"))
+        return fn(*args, **kwargs)
