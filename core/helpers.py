58970a8d323e66538312ca826770183865ef10ea
Jack409M <admin@dwcenter.org>
2026-03-06 21:29:30 -0600

    Update helpers.py

--- core/helpers.py
+++ core/helpers.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
 from typing import Optional
 from flask import g
+from zoneinfo import ZoneInfo
 
 def is_postgres():
     return g.get("db_kind") == "pg"
@@ -21,5 +22,16 @@ def fmt_date(dt_iso: Optional[str]) -> str:
         dt = dt.replace(tzinfo=timezone.utc)
         local_dt = dt.astimezone(ZoneInfo("America/Chicago"))
         return local_dt.strftime("%m/%d/%Y")
+    except Exception:
+        return dt_iso
+
+def fmt_dt(dt_iso: Optional[str]) -> str:
+    if not dt_iso:
+        return ""
+    try:
+        dt = datetime.fromisoformat(dt_iso)
+        dt = dt.replace(tzinfo=timezone.utc)
+        local_dt = dt.astimezone(ZoneInfo("America/Chicago"))
+        return local_dt.strftime("%m/%d/%Y %I:%M %p")
     except Exception:
         return dt_iso
