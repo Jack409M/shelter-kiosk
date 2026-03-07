0bb40cac3e290692c8af3232ef284db89ec3a8e3
Jack409M <admin@dwcenter.org>
2026-03-06 20:24:54 -0600

    Update staff_portal.py

--- routes/staff_portal.py
+++ routes/staff_portal.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template
 from core.auth import require_login, require_shelter
-from flask import session, g, render_template
 
 staff_portal = Blueprint("staff_portal", __name__)
 
