a864ed5cfab26ddc098f10c4e0fd0e40e760dede
Jack409M <admin@dwcenter.org>
2026-03-08 14:39:06 -0500

    Create public.py

---
+++ routes/public.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, redirect, render_template, url_for
+
+
+public = Blueprint("public", __name__)
+
+
+@public.get("/privacy")
+def privacy_policy():
+    return render_template("privacy.html")
+
+
+@public.get("/terms")
+def terms_and_conditions():
+    return render_template("terms.html")
+
+
+@public.route("/")
+def public_home():
+    return redirect(url_for("resident_requests.resident_leave"))
