6bde613eec939c6ab2d708645fa289a034508d52
Jack409M <admin@dwcenter.org>
2026-03-16 18:58:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from core.runtime import init_db
 
 app = create_app()
 
+with app.app_context():
+    init_db()
+
 
 if __name__ == "__main__":
-    with app.app_context():
-        init_db()
-
     app.run(host="127.0.0.1", port=5000)
