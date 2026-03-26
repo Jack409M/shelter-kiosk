48dd3a92c6d398b0a6927a92a0be9e8ec3202879
Jack409M <admin@dwcenter.org>
2026-03-25 22:48:51 -0500

    Update program_statistics.py

--- core/program_statistics.py
+++ core/program_statistics.py
@@ -13,6 +13,7 @@ from core.stats.recovery import (
     get_recovery_and_sobriety,
     get_trauma_and_vulnerability,
 )
+from core.stats.safe import safe_stat
 from core.stats.snapshot import (
     get_capacity_snapshot,
     get_program_snapshot,
@@ -45,35 +46,97 @@ def get_dashboard_statistics(
             "start_date": start_date,
             "end_date": end_date,
         },
-        "program_snapshot": get_program_snapshot(
-            normalized_scope, normalized_population, normalized_date_range, start, end
+        "program_snapshot": safe_stat(
+            "program_snapshot",
+            get_program_snapshot,
+            normalized_scope,
+            normalized_population,
+            normalized_date_range,
+            start,
+            end,
         ),
-        "scope_comparison": get_scope_comparison(
-            normalized_scope, normalized_population, normalized_date_range, start, end
+        "scope_comparison": safe_stat(
+            "scope_comparison",
+            get_scope_comparison,
+            normalized_scope,
+            normalized_population,
+            normalized_date_range,
+            start,
+            end,
         ),
-        "capacity_snapshot": get_capacity_snapshot(),
-        "shelter_distribution": get_shelter_distribution(
-            normalized_population, normalized_date_range, start, end
+        "capacity_snapshot": safe_stat(
+            "capacity_snapshot",
+            get_capacity_snapshot,
         ),
-        "demographics": get_demographics(
-            normalized_scope, normalized_population, normalized_date_range, start, end
+        "shelter_distribution": safe_stat(
+            "shelter_distribution",
+            get_shelter_distribution,
+            normalized_population,
+            normalized_date_range,
+            start,
+            end,
         ),
-        "family_composition": get_family_composition(
-            normalized_scope, normalized_population, normalized_date_range, start, end
+        "demographics": safe_stat(
+            "demographics",
+            get_demographics,
+            normalized_scope,
+            normalized_population,
+            normalized_date_range,
+            start,
+            end,
         ),
-        "recovery_and_sobriety": get_recovery_and_sobriety(
-            normalized_scope, normalized_population, normalized_date_range, start, end
+        "family_composition": safe_stat(
+            "family_composition",
+            get_family_composition,
+            normalized_scope,
+            normalized_population,
+            normalized_date_range,
+            start,
+            end,
         ),
-        "trauma_and_vulnerability": get_trauma_and_vulnerability(
-            normalized_scope, normalized_population, normalized_date_range, start, end
+        "recovery_and_sobriety": safe_stat(
+            "recovery_and_sobriety",
+            get_recovery_and_sobriety,
+            normalized_scope,
+            normalized_population,
+            normalized_date_range,
+            start,
+            end,
         ),
-        "barriers_to_stability": get_barriers_to_stability(
-            normalized_scope, normalized_population, normalized_date_range, start, end
+        "trauma_and_vulnerability": safe_stat(
+            "trauma_and_vulnerability",
+            get_trauma_and_vulnerability,
+            normalized_scope,
+            normalized_population,
+            normalized_date_range,
+            start,
+            end,
         ),
-        "education_and_income": get_education_and_income(
-            normalized_scope, normalized_population, normalized_date_range, start, end
+        "barriers_to_stability": safe_stat(
+            "barriers_to_stability",
+            get_barriers_to_stability,
+            normalized_scope,
+            normalized_population,
+            normalized_date_range,
+            start,
+            end,
         ),
-        "exit_outcomes": get_exit_outcomes(
-            normalized_scope, normalized_population, normalized_date_range, start, end
+        "education_and_income": safe_stat(
+            "education_and_income",
+            get_education_and_income,
+            normalized_scope,
+            normalized_population,
+            normalized_date_range,
+            start,
+            end,
+        ),
+        "exit_outcomes": safe_stat(
+            "exit_outcomes",
+            get_exit_outcomes,
+            normalized_scope,
+            normalized_population,
+            normalized_date_range,
+            start,
+            end,
         ),
     }
