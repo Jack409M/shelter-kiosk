965bc5f1c1f554924d92c94cc44e42cdff7e95e2
Jack409M <admin@dwcenter.org>
2026-04-06 19:26:29 -0500

    Update rent_tracking.py

--- routes/rent_tracking.py
+++ routes/rent_tracking.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from flask import Blueprint
 
+from .rent_tracking_parts.snapshot import build_rent_stability_snapshot
 from .rent_tracking_parts.views import register_routes
 
 
@@ -12,3 +13,8 @@ rent_tracking = Blueprint(
 )
 
 register_routes(rent_tracking)
+
+__all__ = [
+    "rent_tracking",
+    "build_rent_stability_snapshot",
+]
