08994d6b7097b5548f62e240d8779f70eb52ba71
Jack409M <admin@dwcenter.org>
2026-04-13 08:56:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
 
 from core.app_factory import create_app
-from core.runtime import init_db
 
 
 app = create_app()
-
-with app.app_context():
-    init_db()
 
 
 if __name__ == "__main__":
