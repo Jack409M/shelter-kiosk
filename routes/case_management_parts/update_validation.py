5743092d044df4a0b1190781b5a05afbe488cc1a
Jack409M <admin@dwcenter.org>
2026-04-16 14:06:09 -0500

    Create update_validation.py

---
+++ routes/case_management_parts/update_validation.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from routes.case_management_parts.update_note_validation import (
+    has_structured_progress,
+    validate_note_values,
+)
+
+__all__ = [
+    "has_structured_progress",
+    "validate_note_values",
+]
